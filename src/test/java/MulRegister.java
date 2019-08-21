e598f135a6c2d986d4f5aa4278aecaaf24734b39
ibobibo <ibobibo18@gmail.com>
2019-08-21 16:52:58 +0200

    registration data fetching from properties

--- src/test/java/MulRegister.java
+++ src/test/java/MulRegister.java
@@ -7,11 +7,13 @@ import pageObjects.RegisterPage;
 import resources.BaseClass;
 
 import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class MulRegister extends BaseClass {
 
     public static Logger log = LogManager.getLogger(BaseClass.class.getName());
+    public static Properties prop = new Properties();
 
     @BeforeTest
     public void initialize() throws IOException {
@@ -22,7 +24,24 @@ public class MulRegister extends BaseClass {
     public void register() {
         RegisterPage registerPage = new RegisterPage(driver);
 
-        log.info("Successfully Log in with a mul account");
+        registerPage.registerButton().click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        registerPage.siteName().sendKeys(prop.getProperty("registerSiteName"));
+        registerPage.umbrellaOrganisation().click();
+        registerPage.contactSalutation().click();
+        registerPage.contactFirstName().sendKeys(prop.getProperty("contactFirstName"));
+        registerPage.contactLastName().sendKeys(prop.getProperty("contactLastName"));
+        registerPage.contactEmail().sendKeys(prop.getProperty("contactEmail"));
+        registerPage.contactPhone().sendKeys(prop.getProperty("contactPhone"));
+        registerPage.accountFirstPassword().sendKeys(prop.getProperty("contactPassword"));
+        registerPage.accountSecondPassword().sendKeys(prop.getProperty("contactPassword"));
+        registerPage.accountPremiumCode().sendKeys(prop.getProperty("accountPremiumCode"));
+        registerPage.accept().click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        registerPage.registerSubmit().click();
+        //HANDLEDYNAMIC DROPDOWN !!!! VIDEO 47
+
+        log.info("Successfully Register for a mul account");
 
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
     }
