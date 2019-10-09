54d91cb68e5bb306e8413ba210ef32fd1ab05424
Ibrahim Jawad <ibrahim.jawad@sprylab.com>
2019-10-09 17:20:45 +0200

    verifiying mentioned

--- src/test/java/Verify.java
+++ src/test/java/Verify.java
@@ -1,15 +1,13 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import pageObjects.VerifyPage;
 import resources.BaseClass;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class Verify extends BaseClass {
     @BeforeTest
@@ -21,19 +19,16 @@ public class Verify extends BaseClass {
     public void verifyRegisteredUser() {
         Actions actions = new Actions(driver);
 
-        VerifyPage verifyPage = new VerifyPage(driver);
-        WebDriverWait wait = new WebDriverWait(driver, 20);
-
         WebElement findEmail = driver.findElement(By.xpath("//ul[@class='email-list']//span[contains(text(),'test2@test.test0')]"));
         actions.moveToElement(findEmail).click().build().perform();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         try {
-            WebElement waitForFrame = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//iframe[@class='preview-iframe panel-html']"))));
-            driver.switchTo().frame(waitForFrame);
-            System.out.println("switch to frame 0");
-            System.out.println(driver.findElement(By.xpath("/html/body/p[3]/a")).getText());
+            driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='preview-iframe panel-html']")));
+            System.out.println("switch to frame");
+            driver.findElement(By.xpath("/html/body/p[3]/a")).click();
         } catch (Exception e) {
-            System.out.println("doubleshit");
+            System.out.println("shit");
         }
     }
 
