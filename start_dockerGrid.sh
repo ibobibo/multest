56542d3701a8c4ae01c1810f862d66b06c41d3e9
Jonas Bounama <Jonas.Bounama@sprylab.com>
2020-02-24 15:52:05 +0100

    now with script

--- start_dockerGrid.sh
+++ start_dockerGrid.sh
@@ -1,2 +1,9 @@
-echo $PWD
 docker-compose up -d
+mkdir -p /usr/share/maven /usr/share/maven/ref
+curl -fsSL -o /tmp/apache-maven.tar.gz https://apache.osuosl.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+echo "c35a1803a6e70a126e80b2b3ae33eed961f83ed74d18fcd16909b2d44d7dada3203f1ffe726c17ef8dcca2dcaa9fca676987befeadc9b9f759967a8cb77181c0  /tmp/apache-maven.tar.gz" | sha512sum -c -
+tar -xzf /tmp/apache-maven.tar.gz -C /usr/share/maven --strip-components=1
+rm -f /tmp/apache-maven.tar.gz
+ln -s /usr/share/maven/bin/mvn /usr/bin/mvn
+mvn --version
+mvn test
