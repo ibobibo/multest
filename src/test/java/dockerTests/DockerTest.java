67f76a37c2548a5903a1fdaf36683b211bf0661d
Ibrahim Jawad <ibrahim.jawad@sprylab.com>
2020-02-25 11:08:46 +0100

    test

--- src/test/java/dockerTests/DockerTest.java
+++ src/test/java/dockerTests/DockerTest.java
@@ -1,27 +1,13 @@
 package dockerTests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 
 public class DockerTest {
     @Test
     public void dockerTest() throws MalformedURLException, InterruptedException {
-        FirefoxOptions cap = new FirefoxOptions();
-
-        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
-
-        driver.get("http://www.google.com");
-
-        driver.findElement(By.name("q")).sendKeys("Learn Automation");
-
-        Thread.sleep(5000);
-
-        driver.quit();
+        System.out.println("hey");
     }
 }
 
