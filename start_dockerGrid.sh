ad08236c2d8d5343ce9a0a3894b56efe645076bc
Jonas Bounama <Jonas.Bounama@sprylab.com>
2020-03-23 15:27:36 +0100

    new approach

--- start_dockerGrid.sh
+++ start_dockerGrid.sh
@@ -1,2 +1,4 @@
 docker-compose up -d
+name=docker inspect --format='{{.Name}}'
+docker network connect "selenium-headless-network-${CI_JOB_ID}" $name
 docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' selenium-hub
