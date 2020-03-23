9d72dee10a6a5d3bbe6c4ddf4fb6fdbf87f16df0
Jonas Bounama <Jonas.Bounama@sprylab.com>
2020-03-23 20:22:56 +0100

    mental programmer

--- start_dockerGrid.sh
+++ start_dockerGrid.sh
@@ -1,4 +1,5 @@
 docker-compose up -d
+ls
 docker ps
 docker network connect "selenium-headless-network" selenium_test_runner
 docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' selenium-hub
