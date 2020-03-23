91b73048eef2dfd5962b219d34b5143f4182b71f
Jonas Bounama <Jonas.Bounama@sprylab.com>
2020-03-23 17:03:10 +0100

    what is happening

--- start_dockerGrid.sh
+++ start_dockerGrid.sh
@@ -1,6 +1,6 @@
 docker-compose up -d
 name=docker inspect --format='{{.Name}}'
-echo $name
-echo docker ps
+$name
+docker ps
 docker network connect "selenium-headless-network-${CI_JOB_ID}" $name
 docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' selenium-hub
