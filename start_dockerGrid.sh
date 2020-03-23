984d35ebce089d02f4b0615fd87640e29755c987
Jonas Bounama <Jonas.Bounama@sprylab.com>
2020-03-23 19:14:13 +0100

    typos

--- start_dockerGrid.sh
+++ start_dockerGrid.sh
@@ -1,4 +1,4 @@
 docker-compose up -d
-echo ${CI_JOB_ID}
-docker network connect "selenium-headless-network-${CI_JOB_ID}" selenium_test_runner_${CI_JOB_ID}
+echo $CI_JOB_ID
+docker network connect "selenium-headless-network-${CI_JOB_ID}" selenium_test_runner_$CI_JOB_ID
 docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' selenium-hub
