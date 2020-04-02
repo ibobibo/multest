b6d3878d6e6b71ed0a599ee78951ef7a7126b53d
Ibrahim Jawad <ibrahim.jawad@sprylab.com>
2020-04-02 16:34:43 +0200

    start parallel tests5

--- src/test/java/dockerTests/DockerTest.java
+++ src/test/java/dockerTests/DockerTest.java
@@ -11,11 +11,19 @@ public class DockerTest extends BaseClass {
     @Test
     public void dockerTest() throws IOException, InterruptedException {
         initializeBrowser();
-        System.out.println("wieder da");
-
         String x = driver.findElement(By.xpath("//span[contains(text(),'Bitte tragen Sie Ihre Anmeldedaten ein')]")).getText();
         System.out.println("x: " + x);
+
+        //email
+        driver.findElement(By.xpath("//section[2]//div[1]//input[1]")).sendKeys(prop.getProperty("contactEmail"));
+        //pw
+        driver.findElement(By.xpath("//section[4]//div[1]//input[1]")).sendKeys(prop.getProperty("contactPassword"));
+        //accept
+        driver.findElement(By.xpath("//button[@id='login-submit']")).click();
         Thread.sleep(5000);
+
+        String y = driver.findElement(By.xpath("//div[@class='enterpriseName']")).getText();
+        System.out.println("y: " + y);
 
         driver.quit();
     }
