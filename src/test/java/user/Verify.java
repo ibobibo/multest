3ece80baf7d474e440add6c239ec66c87fe295fd
Ibrahim Jawad <ibrahim.jawad@sprylab.com>
2020-04-03 14:57:39 +0200

    start

--- src/test/java/user/Verify.java
+++ src/test/java/user/Verify.java
@@ -2,6 +2,8 @@ package user;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import resources.BaseClass;
 
@@ -13,28 +15,28 @@ public class Verify extends BaseClass {
     @Test
     public void verifyRegisteredUser() throws IOException, InterruptedException {
         initializeMailServer();
-        System.out.println("verifizierung");
+        System.out.println("verzifizierung");
         loadProps();
 
-        for (int i = 0; i < Integer.parseInt(prop.getProperty("counting")); i++) {
-            WebElement inputField = driver.findElement(By.xpath("//div[@class='search-container']//input[@placeholder='Search']"));
-            String x = "//ul[@class='email-list']//span[contains(text(),'" + prop.getProperty("contactEmail").toLowerCase() + i + "')]";
-            inputField.clear();
-            TimeUnit.SECONDS.sleep(1);
-            inputField.sendKeys(prop.getProperty("contactEmail").toLowerCase() + i);
-            TimeUnit.SECONDS.sleep(1);
+        accessAllCookies(driver);
 
+        for (int i = 0; i < Integer.parseInt(prop.getProperty("counting")); i++) {
+            new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder='Search']"))).clear();
+            new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder='Search']"))).sendKeys(prop.getProperty("contactEmail").toLowerCase());
+            TimeUnit.SECONDS.sleep(2);
+
+            String x = "//ul[@class='email-list']//span[contains(text(),'" + prop.getProperty("contactEmail").toLowerCase() + "')]";
             WebElement findEmail = driver.findElement(By.xpath(x));
             findEmail.click();
             TimeUnit.SECONDS.sleep(2);
 
             driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='preview-iframe panel-html']")));
             driver.findElement(By.xpath("/html/body/p[3]/a")).click();
-            TimeUnit.SECONDS.sleep(2);
+            TimeUnit.SECONDS.sleep(4);
 
             ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
             driver.switchTo().window(tabs.get(1));
-            driver.quit();
+            driver.close();
 
             driver.switchTo().window(tabs.get(0));
             TimeUnit.SECONDS.sleep(2);
