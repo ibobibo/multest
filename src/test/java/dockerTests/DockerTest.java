3d5323e9e6322c2389f8917e90156143ba06646d
Ibrahim Jawad <ibrahim.jawad@sprylab.com>
2020-04-03 10:23:40 +0200

    look for props

--- src/test/java/dockerTests/DockerTest.java
+++ src/test/java/dockerTests/DockerTest.java
@@ -11,6 +11,7 @@ public class DockerTest extends BaseClass {
     @Test
     public void dockerTest() throws IOException, InterruptedException {
         initializeBrowser();
+        System.out.println(prop.getProperty("contactEmail")+" :props");
         String x = driver.findElement(By.xpath("//span[contains(text(),'Bitte tragen Sie Ihre Anmeldedaten ein')]")).getText();
         System.out.println("x: " + x);
 
