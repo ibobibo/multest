572f518bddd45a5cdcff8274bd0b438c56c65754
Ibrahim Jawad <ibrahim.jawad@sprylab.com>
2020-04-03 11:01:00 +0200

    timeouts

--- src/test/java/dockerTests/DockerTest.java
+++ src/test/java/dockerTests/DockerTest.java
@@ -5,23 +5,28 @@ import org.testng.annotations.Test;
 import resources.BaseClass;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class DockerTest extends BaseClass {
 
     @Test
     public void dockerTest() throws IOException, InterruptedException {
         initializeBrowser();
+        TimeUnit.SECONDS.sleep(4);
         System.out.println(prop.getProperty("contactEmail")+" :props");
         String x = driver.findElement(By.xpath("//span[contains(text(),'Bitte tragen Sie Ihre Anmeldedaten ein')]")).getText();
+        TimeUnit.SECONDS.sleep(4);
         System.out.println("x: " + x);
 
         //email
         driver.findElement(By.xpath("//section[2]//div[1]//input[1]")).sendKeys(prop.getProperty("contactEmail"));
+        TimeUnit.SECONDS.sleep(4);
         //pw
         driver.findElement(By.xpath("//section[4]//div[1]//input[1]")).sendKeys(prop.getProperty("contactPassword"));
+        TimeUnit.SECONDS.sleep(4);
         //accept
         driver.findElement(By.xpath("//button[@id='login-submit']")).click();
-        Thread.sleep(5000);
+        TimeUnit.SECONDS.sleep(4);
 
         String y = driver.findElement(By.xpath("//div[@class='enterpriseName']")).getText();
         System.out.println("y: " + y);
