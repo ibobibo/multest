2cf463254f9ccf4c87c4445a28bcfef95fed92bb
Ibrahim Jawad <ibrahim.jawad@sprylab.com>
2020-04-07 14:38:19 +0200

    with DesiredCapabilities

--- src/main/java/resources/BaseClass.java
+++ src/main/java/resources/BaseClass.java
@@ -2,7 +2,6 @@ package resources;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
@@ -42,11 +41,17 @@ public class BaseClass {
                 System.out.println("cookies accepted");
             }
         } else if (browserName.equals("firefox")) {
-            System.setProperty(prop.getProperty("firefoxDriver"), prop.getProperty("firefoxDriverPath"));
-            driver = new FirefoxDriver();
+            DesiredCapabilities cap = DesiredCapabilities.firefox();
+            driver = new RemoteWebDriver(new URL("http://" + hubAddress + ":4444/wd/hub"), cap);
             TimeUnit.SECONDS.sleep(4);
             driver.get(prop.getProperty("urlFromHomeNetwork"));
             TimeUnit.SECONDS.sleep(4);
+
+//            System.setProperty(prop.getProperty("firefoxDriver"), prop.getProperty("firefoxDriverPath"));
+//            driver = new FirefoxDriver();
+//            TimeUnit.SECONDS.sleep(4);
+//            driver.get(prop.getProperty("urlFromHomeNetwork"));
+//            TimeUnit.SECONDS.sleep(4);
             try {
                 accessAllCookies(driver);
             } catch (Exception e) {
