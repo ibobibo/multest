91949eadb8cdb5b2085e3f284f683d9af309738b
Ibrahim Jawad <ibrahim.jawad@sprylab.com>
2020-04-24 13:24:56 +0200

    time management3

--- src/main/java/resources/BaseClass.java
+++ src/main/java/resources/BaseClass.java
@@ -2,7 +2,6 @@ package resources;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
@@ -41,11 +40,11 @@ public class BaseClass {
             driver.manage().window().fullscreen();
             driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         } else if (browserName.equals("firefox")) {
-            System.setProperty(prop.getProperty("firefoxDriver"), prop.getProperty("firefoxDriverPath"));
-            driver = new FirefoxDriver();
+//            System.setProperty(prop.getProperty("firefoxDriver"), prop.getProperty("firefoxDriverPath"));
+//            driver = new FirefoxDriver();
 
-//            DesiredCapabilities cap = DesiredCapabilities.firefox();
-//            driver = new RemoteWebDriver(new URL("http://" + hubAddress + ":4444/wd/hub"), cap);
+            DesiredCapabilities cap = DesiredCapabilities.firefox();
+            driver = new RemoteWebDriver(new URL("http://" + hubAddress + ":4444/wd/hub"), cap);
 
             driver.manage().window().fullscreen();
             driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
