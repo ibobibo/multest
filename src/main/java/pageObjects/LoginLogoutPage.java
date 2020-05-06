6b3d1df528d1ad6446b20d968569efe21751070f
Ibrahim Jawad <ibrahim.jawad@sprylab.com>
2020-05-06 16:33:29 +0200

    without whole filepath

--- src/main/java/pageObjects/LoginLogoutPage.java
+++ src/main/java/pageObjects/LoginLogoutPage.java
@@ -16,9 +16,9 @@ public class LoginLogoutPage {
     //LOGIN
     @FindBy(xpath = "//section[@id='app-header']//a[@href='/login']")
     WebElement anmeldenButton;
-    @FindBy(xpath = "//section[2]//div[1]//input[1]")
+    @FindBy(xpath = "//section[2]//div[1]//input[@id='username']")
     WebElement username;
-    @FindBy(xpath = "//section[4]//div[1]//input[1]")
+    @FindBy(xpath = "//section[4]//div[1]//input[@id='password']")
     WebElement password;
     @FindBy(xpath = "//button[@id='login-submit']")
     WebElement anmelden;
